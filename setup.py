6239b28678a027e34bd7960a8fa4415b2f9680e8
Gregory Ashton <gash789@gmail.com>
2015-04-21 10:02:59 +0100

    Attemts to get the relevant files in place for PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(name='checkgit',
+      packages=['checkgit'],
+      version='0.1',
+      description='A quick visual check of several git repositories',
+      author='Greg Ashton',
+      author_email='gash789@gmail.com',
+      url='https://github.com/ga7g08/CheckGit',
+      download_url='https://github.com/ga7g08/CheckGit/tarball/0.1',
+      keywords=['git'],
+      classifiers=[],
+      scripts=['CheckGit']
+      )
